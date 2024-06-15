95dcd9f74374134e26081f516ce50076dacb9484
Jayanth-BS <109243649+Jayanth-BS@users.noreply.github.com>
2024-06-15 14:37:13 +0530

    Time: 13 ms (8.37%) | Memory: 18.1 MB (96.06%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -1,36 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (root == nullptr) return 0;
-        
-        queue<pair<TreeNode*, unsigned long long>> q;
-        q.push({root, 1});
-        int mx = 0;
-        
-        while (!q.empty()) {
-            int s = q.size();
-            unsigned long long f = 0, b = 0;
-            unsigned long long offset = q.front().second;
-            
-            for (int i = 0; i < s; ++i) {
+        queue<pair<TreeNode*,long long>> q;
+        long long mx = 1;
+        long long mod = 1e11;
+        if(root!=NULL)
+        {
+            q.push({root,1});
+        }
+        while(!q.empty()){
+            long long s = q.size(),f=0,b=0;
+            for(int i=0;i<s;i++){
+                if(i == 0)
+                    f = q.front().second;
+                if(i == s-1)
+                    b = q.front().second;
+                
                 auto k = q.front();
                 q.pop();
-                
-                unsigned long long cur_index = k.second - offset; 
-                
-                if (i == 0) f = cur_index;
-                if (i == s - 1) b = cur_index;
-                
-                if (k.first->left != nullptr) {
-                    q.push({k.first->left, 2 * cur_index});
-                }
-                if (k.first->right != nullptr) {
-                    q.push({k.first->right, 2 * cur_index + 1});
-                }
+                if(k.first->left != NULL){
+                    q.push({k.first->left,(2*k.second)%mod});
+                }                
+                if(k.first->right != NULL){
+                    q.push({k.first->right,(2*k.second+1)%mod});
+                }  
             }
-            mx = max(static_cast<int>(b - f + 1), mx);
+            mx = max(b-f+1,mx);
         }
         return mx;
      }
 };
-
