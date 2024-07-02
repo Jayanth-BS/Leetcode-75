a0c4d6d22ff98484dfbdc195019962e222571e19
Jayanth-BS <109243649+Jayanth-BS@users.noreply.github.com>
2024-07-02 15:36:52 +0530

    Time: 17 ms (26.60%) | Memory: 19.9 MB (31.14%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
-public:
-    int f(int i,int amt, vector<int>& coins,vector<vector<int>> &dp){
-        if(i ==0){
-            return (amt%coins[0]==0);
-        }
-        if(dp[i][amt]!= -1)
-        return dp[i][amt];
-        int ntake = f(i-1, amt, coins,dp);
-        int  take =0;
-        if(amt>=coins[i])
-        take= f(i,amt-coins[i], coins,dp);
-        return dp[i][amt] = take + ntake;
-    }
+public: // tabulation
     int change(int amt, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size()+1,vector<int>(amt+1,-1));
-        return f(coins.size()-1,amt,coins,dp);
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amt+1,0));
+        for(int t =0;t<=amt;t++)
+        dp[0][t] = (t%coins[0]==0);
+
+        for(int i=1;i<coins.size();i++){
+            for(int j = 0;j<=amt;j++){
+                    int ntake = dp[i-1][j];
+                    int  take =0;
+                    if(j>=coins[i])
+                        take= dp[i][j-coins[i]];
+                   dp[i][j] = take + ntake;
+            }
+        }
+        return dp[coins.size()-1][amt];
     }
 };
 
 
-//Recursion
+//Recursion & do memoization for this
     // int f(int i,int amt, vector<int>& coins){
     //     if(i ==0){
     //         return (amt%coins[0]==0);
