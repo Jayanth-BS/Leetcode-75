768645d28c8685931c5528f6a27e99a25ea1814d
Jayanth-BS <109243649+Jayanth-BS@users.noreply.github.com>
2024-07-03 02:07:13 +0530

    Time: 4 ms (71.53%) | Memory: 15.8 MB (35.07%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int t[30004][2];
-    int f(int i, int buy,vector<int>& prices){
-        if(t[i][buy]!= -1)
-        return t[i][buy];
-
-        if(i == prices.size()) 
-        return 0;
-       
-        int profit=0;//as profit = sp-cp..so sub in buying and add in selling
-        if(buy){  //case where u have option to buy
-            profit = max(-prices[i] + f(i+1,0,prices), 0 + f(i+1,1,prices)); // buy or keep
-        }
-        else{//case where u have option to sell
-            profit = max(prices[i] + f(i+1,1,prices), 0 + f(i+1,0,prices)); //sell or keep
-        }
-        return t[i][buy]= profit;
-    }
     int maxProfit(vector<int>& prices) {
         memset(t,-1,sizeof(t));
-        return f(0,1,prices);
+        int n = prices.size();
+        t[n][0]=0;
+        t[n][1]=0;
+        for(int i = n-1;i>=0;i--){
+                int profit=0;
+                t[i][1] = max(-prices[i] + t[i+1][0], 0 + t[i+1][1]); 
+                t[i][0] = max(prices[i] + t[i+1][1], 0 + t[i+1][0]);
+        }
+        return t[0][1];
     }
 };
