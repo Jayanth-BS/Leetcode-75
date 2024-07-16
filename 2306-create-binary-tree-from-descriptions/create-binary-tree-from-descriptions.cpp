8d501edbe79348ddd3b3b1681b7efab36b60c215
Jayanth-BS <109243649+Jayanth-BS@users.noreply.github.com>
2024-07-16 20:47:22 +0530

    Time: 767 ms (33.19%) | Memory: 291.9 MB (35.13%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -12,35 +12,21 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int,vector<pair<int,int>>> map;
-        set<int> par, child;
-        for(auto i:descriptions){
-            map[i[0]].push_back({i[1],i[2]});
-            par.insert(i[0]);
-            child.insert(i[1]);
+        unordered_map<int,TreeNode*> map;
+        for(auto d: descriptions){
+            map[d[1]] = new TreeNode(d[1]);
         }
-        int val ;
-        for(auto i:par){
-                if(!child.count(i)){
-                    val = i;
-                }
-        }
-        TreeNode* root = new TreeNode(val);
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            TreeNode* temp = q.front(); q.pop();
-            for(auto i: map[temp->val]){
-                 TreeNode* node = new TreeNode(i.first);
-                if(i.second == 1)
-                    temp->left = node;
-                
-                if(i.second == 0)
-                     temp->right = node;
-                
-                q.push(node);
+        TreeNode* root=nullptr;
+        for(auto d:descriptions){
+            if(!map[d[0]]){
+                map[d[0]] = new TreeNode(d[0]);
+                root = map[d[0]];
+                break;
             }
+        }
+        for(auto d: descriptions){
+            if(d[2]) map[d[0]]->left = map[d[1]];
+            else map[d[0]]->right = map[d[1]];
         }
         return root;
     }
